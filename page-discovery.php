04fd8214c478455a6be65fd87f722f9f12852b5a
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2019-12-04 22:22:41 -0300

    Discovery

---
+++ page-discovery.php
@@ -0,0 +1,39 @@
+<?php $page_name = "discovery" ?>
+<?php include('header.php'); ?>
+
+<section class="discovery">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center heading">
+                <h2 class="title">Áreas de discovery</h2>
+            </div>
+            <div class="col-md-12 text-center">
+                <div class="circle-discovery">
+                    <img src="img/areas-discovery.png" class="img-responsive">
+                    <a href="#" class="circle-negocios">
+                        <p>Negocios</p>
+                        <img src="img/circle-negocios.png">
+                    </a>
+                    <a href="#" class="circle-personas">
+                        <img src="img/circle-personas.png">
+                        <p>Personas</p>
+                    </a>
+                    <a href="#" class="circle-ti">
+                        <img src="img/circle-ti.png">
+                        <p>Ti</p>
+                    </a>
+                    <a href="#" class="circle-operaciones">
+                        <img src="img/circle-operaciones.png">
+                        <p>Operaciones y <br>Mejoras de Procesos</p>
+                    </a>
+                    <a href="#" class="circle-transformacion">
+                        <img src="img/circle-transformacion.png">
+                        <p>Transformación <br>Digital</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
